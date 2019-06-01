21f5236b8c321c46a44880da25caac1a57f3bcb7
albatovska <tanya.goncharenko@gmail.com>
2019-06-01 09:54:18 +0300

    add chenge

--- HomeWork14/videoplayer/js/video_player.js
+++ HomeWork14/videoplayer/js/video_player.js
@@ -1,121 +1,40 @@
 class VideoPlayerBasic {
     constructor(settings) {
         // "сольем" наши дефолтные сетинги с теми, что пришли
-        this.settings = Object.assign(VideoPlayerBasic.getDefaultSettings(), settings)
+        this.settings = Object.assign(
+            VideoPlayerBasic.getDefaultSettings(),
+            settings
+        );
     }
 
     init() {
         // сделаем проверку на то, что пришли в настройках непустые строки
-        if (!this.settings.videoUrl) return console.error('NOT videoUrl')
-        if (!this.settings.videoPlayerContainer) return console.error('NOT videoPlayerContainer')
+        if (!this.settings.videoUrl) return console.error("NOT videoUrl");
+        if (!this.settings.videoPlayerContainer)
+            return console.error("NOT videoPlayerContainer");
 
         // создадим разметку на странице
-        this.addTemplate()
-            // найдем эл-ты для управления видосиком
-        this.setElements()
-            // обработчики
-        this.setEvents()
+        this.addTemplate();
+        // найдем эл-ты для управления видосиком
+        this.setElements();
+        // обработчики
+        this.setEvents();
     }
 
     addTemplate() {
-        const template = this.createTemplate()
-        const container = document.querySelector(this.settings.videoPlayerContainer)
+        const template = this.createTemplate();
+        const container = document.querySelector(
+            this.settings.videoPlayerContainer
+        );
 
-        container ? container.insertAdjacentHTML('afterbegin', template) : console.error('NOT videoPlayerContainer')
-    }
-
-    setElements() {
-        this.container = document.querySelector(this.settings.videoPlayerContainer);
-        this.video = this.container.querySelector('video');
-        this.toggleBtn = this.container.querySelector('.toggle');
-        this.progress = this.container.querySelector('.progress__filled');
-        this.progressContainer = this.container.querySelector('.progress');
-
-        this.playerSlider = this.container.querySelector('.player__slider');
-        this.playerSliderSpeed = this.container.querySelector('.player__slider__speed');
-        this.playerSkip = this.container.querySelectorAll('.player__button');
-
-    }
-
-    setEvents() {
-        this.video.addEventListener('click', this.toggleVideo);
-        this.toggleBtn.addEventListener('click', this.toggleVideo);
-        this.progressContainer.addEventListener('click', (e) => this.scrub(e));
-        this.progressContainer.addEventListener('mousedown', () => this.isMouseDown = true);
-        this.progressContainer.addEventListener('mouseup', () => this.isMouseDown = false);
-        this.progressContainer.addEventListener('mousemove', (e) => {
-            this.isMouseDown && this.scrub(e)
-        });
-        this.video.addEventListener('timeupdate', this.handleProgress);
-
-        // this.playerSlider.addEventListener('click', (e) => this.setValume);
-        // this.playerSlider.addEventListener('mousedown', () => this.isMouseDown = true);
-        // this.playerSlider.addEventListener('mouseup', () => this.isMouseDown = false);
-        // this.playerSlider.addEventListener('mousemove', (e) => {
-        //     this.isMouseDown && this.setValume(event)
-        // });
-
-        // this.playerSliderSpeed.addEventListener('click', (e) => this.setSpeed);
-        // this.playerSliderSpeed.addEventListener('mousedown', () => this.isMouseDown = true);
-        // this.playerSliderSpeed.addEventListener('mouseup', () => this.isMouseDown = false);
-        // this.playerSliderSpeed.addEventListener('mousemove', (e) => {
-        //     this.playerSliderSpeed && this.setSpeed(event)
-        // });
-
-        this.playerSlider.addEventListener('change', this.setValume);
-        this.playerSliderSpeed.addEventListener('click', this.setSpeed);
-        // this.playerSkip.addEventListener('click', (e) => this.setSkip(e));
-        this.playerSkip.forEach(element => {
-            element.addEventListener('click', () => {
-                this.skip(element.getAttribute('data-skip'))
-            })
-        });
-        this.video.addEventListener('dblclick', (e) => this.setSpeed(e))
-    }
-
-    setSkip = (e) => {
-        if (this.playerSkip.dataset.skip = '1') {
-            this.video.currentTime = +e.value
-        } else if (this.playerSkip.dataset.skip = '-1') {
-            this.video.currentTime = -e.value
-        }
-    }
-    setValume = (event) => {
-        this.video.volume = event.target.value * 0.1
-    }
-    setSpeed = (event) => {
-        if (event.clientX > 600) {
-            this.video.currentTime += 2;
-        } else {
-            this.video.currentTime -= 2;
-        }
-
-    }
-
-    toggleVideo = () => {
-        const method = this.video.paused ? 'play' : 'pause';
-        this.toggleBtn.textContent = this.video.paused ? ' || ' : ' ► ';
-        this.video[method]()
-    }
-
-    scrub(e) {
-        this.video.currentTime = (e.offsetX / this.progressContainer.offsetWidth) * this.video.duration
-    }
-
-    handleProgress = () => {
-        const {
-            currentTime,
-            duration
-        } = this.video
-        const percent = (currentTime / duration) * 100
-        this.progress.style.flexBasis = `${percent}%`
+        container
+            ?
+            container.insertAdjacentHTML("afterbegin", template) :
+            console.error("NOT videoPlayerContainer");
     }
 
     createTemplate() {
-        const {
-            videoUrl,
-            volume
-        } = this.settings;
+        const { videoUrl, volume, skipPrev, skipNext } = this.settings;
 
         return `
         <div class="player">
@@ -126,27 +45,111 @@ class VideoPlayerBasic {
             </div>
             <button class="player__button toggle" title="Toggle Play">►</button>
             <input type="range" name="volume" class="player__slider" min=0 max="1" step="0.05" value="${volume}">
-            <input type="range" name="playbackRate" class="player__slider" min="0.5" max="2" step="0.1" value="1">
-            <button data-skip="-1" class="player__button">« 1s</button>
-            <button data-skip="1" class="player__button">1s »</button>
+            <input type="range" name="playbackRate" class="player__slider" min="0.5" max="5" step="0.1" value="1">
+            <button data-skip="-${skipPrev}" class="player__button">«${skipPrev}s</button>
+            <button data-skip="${skipNext}" class="player__button">${skipNext}s »</button>
             </div>
         </div>
-        `
+        `;
     }
 
+    setElements() {
+        this.container = document.querySelector(this.settings.videoPlayerContainer);
+        this.video = this.container.querySelector("video");
+        this.toggleBtn = this.container.querySelector(".toggle");
+        this.progress = this.container.querySelector(".progress__filled");
+        this.progressContainer = this.container.querySelector(".progress");
+        this.volumeRange = this.container.querySelector("[name = 'volume']");
+        this.playbackRate = this.container.querySelector("[name = 'playbackRate']");
+        this.skips = this.container.querySelectorAll("[data-skip]");
+    }
+
+    setEvents() {
+        this.video.addEventListener("click", this.toggleVideo);
+        this.video.addEventListener("dblclick", e => this.handleDbclick(e));
+        this.toggleBtn.addEventListener("click", this.toggleVideo);
+        this.progressContainer.addEventListener("click", e => this.scrub(e));
+        this.progressContainer.addEventListener(
+            "mousedown",
+            () => (this.isMouseDown = true)
+        );
+        this.progressContainer.addEventListener(
+            "mouseup",
+            () => (this.isMouseDown = false)
+        );
+        this.progressContainer.addEventListener("mousemove", e => {
+            this.isMouseDown && this.scrub(e);
+        });
+        this.video.addEventListener("timeupdate", this.handleProgress);
+        this.volumeRange.addEventListener("change", this.changeVolume);
+        this.playbackRate.addEventListener("change", this.changePlaybackRate);
+        this.playbackRate.addEventListener("change", this.handlePlaybackRate);
+        Array.prototype.forEach.call(this.skips, e =>
+            e.addEventListener("click", evt => this.handleSkip(evt))
+        );
+    }
+
+    handleDbclick(e) {
+        e.stopPropagation();
+        const halfWidth = this.video.offsetWidth / 2;
+        if (e.offsetX > halfWidth) {
+            this.addCurrentTime(this.settings.skipNext);
+        } else {
+            this.addCurrentTime(-this.settings.skipPrev);
+        }
+    }
+
+    changeVolume = event => {
+        this.video.volume = event.target.value;
+    };
+
+    changePlaybackRate = event => {
+        this.video.playbackRate = event.target.value;
+    };
+
+    toggleVideo = () => {
+        const method = this.video.paused ? "play" : "pause";
+        this.toggleBtn.textContent = this.video.paused ? " || " : " ► ";
+        this.video[method]();
+    };
+
+    scrub(e) {
+        this.video.currentTime =
+            (e.offsetX / this.progressContainer.offsetWidth) * this.video.duration;
+    }
+
+    handleProgress = () => {
+        const { currentTime, duration } = this.video;
+        const percent = (currentTime / duration) * 100;
+        this.progress.style.flexBasis = `${percent}%`;
+    };
+    handlePlaybackRate() {
+        this.video.playbackRate = this.playbackRate.value;
+    }
+
+    handleSkip(e) {
+        const value = +e.target.dataset.skip;
+        this.addCurrentTime(value);
+    }
+    addCurrentTime(value) {
+        this.video.currentTime += value;
+    }
     static getDefaultSettings() {
         return {
-            videoUrl: '',
-            videoPlayerContainer: '',
+            videoUrl: "",
+            videoPlayerContainer: "",
             volume: 1,
-            playbackRate: 1
-        }
+            skipPrev: 1,
+            skipNext: 1
+        };
     }
 }
 
 const myPlayer = new VideoPlayerBasic({
-    videoUrl: 'video/mov_bbb.mp4',
-    videoPlayerContainer: 'body',
+    videoUrl: "video/mov_bbb.mp4",
+    videoPlayerContainer: "body",
+    skipPrev: 2,
+    skipNext: 2
 });
 
 myPlayer.init();
